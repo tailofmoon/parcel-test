4fc65987e8f128a69eadb1f9cb8c4b35ad714e67
tailofmoon <tailofmoon@naver.com>
2019-08-19 14:11:13 +0900

    '333'

--- dist/js.00a46daa.js
+++ dist/js.00a46daa.js
@@ -10985,7 +10985,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "7843" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "10507" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
